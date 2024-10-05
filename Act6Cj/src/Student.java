1ed94b17774d88e5a8561b2952520d0052c6f016
David Alejandro Tercero Caballero <datc0218@gmail.com>
2024-10-04 20:23:30 -0600

    Actividad 6 sin static

---
+++ Act6Cj/src/Student.java
@@ -0,0 +1,64 @@
+public class Student extends Person {
+
+    public int[] grades;
+    private int average;
+
+    public Student(String name, int age, int identificador, String campus, int[] grades) {
+        super(name, age, identificador, campus);
+        this.grades = grades;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    public int getAverage() {
+        return average;
+    }
+
+    public void setAverage(int average) {
+        this.average = average;
+    }
+
+    public double calculateAverage() {
+        int suma = 0;
+        for (int i = 0; i < grades.length; i++) {
+            suma = suma + grades[i];
+        }
+        return (double) suma / grades.length;
+    }
+
+    public char setFinalGrade() {
+        if (average <= 50) {
+            return 'F';
+        } else if (average <= 60) {
+            return 'E';
+        } else if (average <= 70) {
+            return 'D';
+        } else if (average <= 80) {
+            return 'C';
+        } else if (average <= 90) {
+            return 'B';
+        } else {
+            return 'A';
+        }
+    }
+    public void studentInfo (){
+        double average = calculateAverage();
+        char finalGrade = setFinalGrade();
+        System.out.println("Nombre: " + getName());
+        System.out.println("Edad: " + getAge());
+        System.out.println("Id: " + getIdentificador());
+
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println("Calificación " + (i + 1) + ": " + grades[i]);
+        }
+
+        System.out.println("El promedio es: " + average);
+        System.out.println("La calificación final es: " + finalGrade);
+    }
+}
